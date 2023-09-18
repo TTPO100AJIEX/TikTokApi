4097ebf4f2a9dc92e54bd9fddfe56cd5bf169a06
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-09-18 21:18:04 +0300

    Logs

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -4,5 +4,5 @@ import config from "../config.json" assert { type: "json" };
 const options = { serverApi: { version: mongodb.ServerApiVersion.v1, strict: true, deprecationErrors: true } };
 const connection = new mongodb.MongoClient(config.mongoDB.connection, options);
 
-connection.connect();
+connection.connect().then(() => console.log(`Started mongodb`));
 export default connection.db(config.mongoDB.database).collection("gifts");
